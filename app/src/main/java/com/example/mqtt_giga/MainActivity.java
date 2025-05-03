f897a025e71eb393a09baa1413c799fae927ec25
SiberK <siberk@mail.ru>
2025-05-03 22:43:37 +0700

    worked

--- app/src/main/java/com/example/mqtt_giga/MainActivity.java
+++ app/src/main/java/com/example/mqtt_giga/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.mqtt_giga;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.IntentFilter;
+import android.content.res.ColorStateList;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
@@ -11,9 +14,14 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.graphics.drawable.DrawableCompat;
+
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -47,7 +55,6 @@ public class MainActivity extends AppCompatActivity
     private BroadcastReceiver receiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, @NonNull Intent intent) {
-            // Обновление UI с полученным сообщением
             TextView textView = findViewById(R.id.tvTopic)    ; textView.setText(intent.getStringExtra("topic"));
             textView = findViewById(R.id.tvMsg)               ; textView.setText(intent.getStringExtra("message"));
             textView = findViewById(R.id.tvStat)              ; textView.setText(intent.getStringExtra("state"));
@@ -108,17 +115,6 @@ public class MainActivity extends AppCompatActivity
         super.onDestroy();
         unregisterReceiver(receiver); // Отмена регистрации ресивера
     }
-
-    //---------------------------------------------------------------
-//    private String getMacAddress(Context context) {
-//        String mac = "" ;
-//        WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
-//        if (wifiManager != null) {
-//            WifiInfo wifiInfo = wifiManager.getConnectionInfo();
-//            mac = wifiInfo.getMacAddress()      ;
-//        }
-//        return mac;
-//    }
     //---------------------------------------------------------------
     private void showSoundPicker() {
         Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
@@ -148,8 +144,7 @@ public class MainActivity extends AppCompatActivity
         tvSound.setText(strSound)       ;
     }
     //---------------------------------------------------------------
-    // Сохранение настроек
-    private void saveSettings() {
+    private void saveSettings() {                       // Сохранение настроек
         if(uriSound != null)  strSound = uriSound.toString()    ;
         getSharedPreferences("MQTT_SETTINGS", MODE_PRIVATE)
                 .edit()
@@ -209,20 +204,61 @@ public class MainActivity extends AppCompatActivity
         Toast.makeText(MainActivity.this, "Сервис остановлен", Toast.LENGTH_SHORT).show();
     }
     //---------------------------------------------------------------
-
     @Override
     public void onAccountSelected(Account _account) {
         account = _account  ;
         tvAccount.setText(account.toString());
     }
-
+    //---------------------------------------------------------------
     @Override
     public void onAccountListDismissed() {
         // Можно добавить дополнительную логику при закрытии списка
     }
-
+    //---------------------------------------------------------------
     @Override
     public void onAccountUpdated() {
         // Обновление UI при изменении аккаунтов
     }
+    //---------------------------------------------------------------
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+
+        // Получаем ColorStateList
+        ColorStateList colorStateList = ContextCompat.getColorStateList(this, R.color.icon_color_selector);
+
+        // Применяем к каждой иконке
+        for (int i = 0; i < menu.size(); i++) {
+            MenuItem item = menu.getItem(i);
+            Drawable icon = item.getIcon();
+            if (icon != null) {
+                icon = DrawableCompat.wrap(icon);
+                DrawableCompat.setTintList(icon, colorStateList);
+                item.setIcon(icon);
+            }
+        }
+        return true;
+    }
+    //---------------------------------------------------------------
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        boolean rzlt = super.onOptionsItemSelected(item);
+        // Обработка нажатий на пункты меню
+        if(item.getItemId() == R.id.action_settings){
+            Toast.makeText(this, "action_settings", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(this, SettingsActivity.class));
+            rzlt = true   ;
+        }
+        else if(item.getItemId() == R.id.action_search){
+            Toast.makeText(this, "action_search", Toast.LENGTH_SHORT).show();
+            rzlt = true   ;
+        }
+        else if(item.getItemId() == R.id.action_help){
+            Toast.makeText(this, "action_help", Toast.LENGTH_SHORT).show();
+            rzlt = true   ;
+        }
+    return rzlt ;
+    }
+    //---------------------------------------------------------------
 }
